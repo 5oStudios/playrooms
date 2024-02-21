d95237284d3b037eb1ebefd3647570c59d336f78
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-21 10:24:43 +0200

    feat: improve colors
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/src/components/game.tsx
+++ apps/frontend/src/components/game.tsx
@@ -8,10 +8,13 @@ import {
   useMultiplayerState,
   usePlayersList,
 } from 'playroomkit';
-import { Card, CardBody, CardHeader } from '@nextui-org/react';
+import { Button, Card, CardBody, CardHeader } from '@nextui-org/react';
 import questions from '../../mocks/questions.json';
 import { toast } from 'sonner';
 import Countdown from 'react-countdown';
+import { AnimatedTooltip } from './ui/animated-tooltip';
+import * as process from 'process';
+import { cn } from '../utils/cn';
 
 export default function Game() {
   const [isGameStarted, setIsGameStarted] = useState(false);
@@ -69,7 +72,11 @@ export default function Game() {
     onPlayerJoin(handlePlayerJoin);
   }, []);
 
-  if (currentQuestion === questions.length - 1 || timer === 0 || isGameEnded) {
+  if (
+    currentQuestion === questions.length - 1 ||
+    timer === 0 ||
+    (isGameEnded && process.env.NODE_ENV !== 'development')
+  ) {
     return <div>Game Over</div>;
   }
   if (!isGameStarted) {
@@ -78,21 +85,23 @@ export default function Game() {
 
   return (
     <div>
-      {currentPlayers.map((player) => (
-        <div key={player.id}>
-          <h1>{player.getProfile().name}</h1>
-          <h2>{player.getState('score')}</h2>
-        </div>
-      ))}
+      {/*<h1>{player.getProfile().name}</h1>*/}
+      {/*<h2>{player.getState('score')}</h2>*/}
+      <div className="flex gap-8">
+        <AnimatedTooltip
+          items={currentPlayers.map((player) => ({
+            id: player.id,
+            name: player.getProfile().name,
+            designation: `Score: ${player.getState('score')}`,
+            image: player.getProfile().photo,
+          }))}
+        />
+      </div>
       <h1 className="text-4xl font-bold mb-4">
         score: {myPlayer().getState('score')}
       </h1>
       <div>
         <h1 className="text-4xl font-bold mb-4">Quiz Game</h1>
-        <p className="text-lg">
-          Welcome to the quiz game! You have 60 seconds to answer as many
-          questions as you can.
-        </p>
         <p className="text-lg">
           Time remaining:{' '}
           <span className="font-bold" id="score">
@@ -139,16 +148,21 @@ const Question: React.FC<Props> = ({ questionData, onClick }) => {
   return (
     <Card
       isBlurred
-      className="border-none bg-background/40 dark:bg-default-100/40 max-w-[610px] p-8 rounded-lg backdrop-blur-[2px]"
+      className="border-none bg-neutral-950/60 dark:bg-default-100/45 max-w-[610px] p-8 rounded-lg backdrop-blur-[2px]"
       shadow="sm"
     >
-      <CardHeader className="flex gap-3 mb-3">
+      <CardHeader className="flex gap-3 mb-6">
         <h2 className="text-2xl font-bold">{question}</h2>
       </CardHeader>
       <CardBody>
         <div className="grid grid-cols-2 gap-4">
           {answers.map((answer, index) => (
-            <Answer key={index} answer={answer} onClick={onClick} />
+            <Answer
+              key={index}
+              answer={answer}
+              onClick={onClick}
+              index={index}
+            />
           ))}
         </div>
       </CardBody>
@@ -157,26 +171,59 @@ const Question: React.FC<Props> = ({ questionData, onClick }) => {
 };
 
 const Answer: React.FC<{
+  index: number;
   answer: Answer;
   onClick: (answer: Answer) => void;
-}> = ({ answer, onClick }) => {
+}> = ({ answer, onClick, index }) => {
+  const [playersChooseThis, setPlayersChooseThis] = useMultiplayerState(
+    `playersChooseThis${index}`,
+    []
+  );
+  const myPhoto = myPlayer().getProfile().photo;
+
+  let abbreviation = '';
+  switch (index) {
+    case 0:
+      abbreviation = 'A';
+      break;
+    case 1:
+      abbreviation = 'B';
+      break;
+    case 2:
+      abbreviation = 'C';
+      break;
+    case 3:
+      abbreviation = 'D';
+      break;
+  }
   return (
     <div>
-      <button
-        className="w-full p-4 rounded-lg bg-background/20 dark:bg-default-100/20 text-lg font-bold"
+      <Button
+        size="lg"
+        disableRipple={true}
+        className={cn(
+          'w-full rounded-lg bg-neutral-950/90 dark:bg-default-100/20 text-lg font-bold flex text-center items-center justify-center overflow-hidden relative shadow-md transition duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-sky-500 focus:ring-offset-neutral-950/90 dark:focus:ring-offset-default-100/20',
+          {}
+        )}
         style={{ backdropFilter: 'blur(4px)' }}
         onClick={() => onClick(answer)}
       >
-        {answer.option}
-      </button>
+        <span className="text-2xl font-bold left-0 px-4 py-2 top-0 text-center bg-gradient-to-r from-emerald-500 to-sky-500 text-transparent bg-clip-text">
+          {abbreviation}
+        </span>
+        <span className="flex-1">{answer.option}</span>
 
-      <div className="flex gap-1">
-        {/*<img*/}
-        {/*  className="w-8 h-8 rounded-full"*/}
-        {/*  src={myPhoto}*/}
-        {/*  alt="Your profile"*/}
-        {/*/>*/}
-        {/*<span className="text-sm">{myPlayer().getProfile().name}</span>*/}
+        <div className="absolute inset-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent w-px" />
+      </Button>
+      <div className={cn('flex gap-2 mt-2')}>
+        {playersChooseThis.map((playerId) => (
+          <img
+            key={playerId}
+            src={myPhoto}
+            alt="player"
+            className="h-8 w-8 rounded-full"
+          />
+        ))}
       </div>
     </div>
   );
