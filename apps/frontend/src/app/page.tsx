068cc3fa7a07d4742ce8ffbaf882bf3428f8410f
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-21 07:45:37 +0200

    fix: ts-expect-error async lazy import component
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 import React from 'react';
 import Game from '../components/game';
-// @ts-expect-error to be solved
+
 const LazyLoadedGame = dynamic(async () => Game, {
   ssr: false,
 });
