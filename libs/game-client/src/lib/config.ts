60a2ca245111daf2bf3c668141d6857c2e286acc
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-02-28 17:16:11 +0200

    Update config.ts

--- libs/game-client/src/lib/config.ts
+++ libs/game-client/src/lib/config.ts
@@ -6,7 +6,7 @@ enum NODE_ENV_STATE {
 }
 export const SERVER_KEY = 'defaultkey',
   HOST = '172.19.0.4',
-  PORT = '7350',
+  PORT = '443',
   USE_SSL = true,
   TIMEOUT = 7500,
   AUTO_REFRESH_SESSION = true;
