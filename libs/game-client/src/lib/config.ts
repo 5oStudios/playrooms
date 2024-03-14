57766232eb3c8c84d9b77bc28cd55d24b9d983c1
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-03-14 23:47:23 +0200

    feat: env

--- libs/game-client/src/lib/config.ts
+++ libs/game-client/src/lib/config.ts
@@ -6,7 +6,7 @@ export enum NODE_ENV_STATE {
 }
 export const SERVER_KEY = 'defaultkey',
   // HOST = 'nakama.5ostudios.com',
-  HOST = process.env['NAKAMA_HOST'] || 'localhost',
+  HOST = process.env['NAKAMA_HOST'] || 'nakama',
   PORT = process.env['NAKAMA_PORT'] || '7350',
   USE_SSL = process.env['NAKAMA_SSL'] === 'true',
   TIMEOUT = 7500,
