0d1ac196fa5a3a8bb13b1de0bc5463451dca8750
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-03-15 01:08:24 +0200

    fix: build
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- libs/game-client/src/lib/config.ts
+++ libs/game-client/src/lib/config.ts
@@ -6,8 +6,8 @@ export enum NODE_ENV_STATE {
 }
 export const SERVER_KEY = 'defaultkey',
   // HOST = 'nakama.5ostudios.com',
-  HOST = process.env['NAKAMA_HOST'] || 'nakama',
-  PORT = process.env['NAKAMA_PORT'] || '27350',
+  HOST = process.env['NAKAMA_HOST'] || 'nakama.5ostudios.com',
+  PORT = process.env['NAKAMA_PORT'] || '',
   USE_SSL = true,
   TIMEOUT = 7500,
   AUTO_REFRESH_SESSION = true;
