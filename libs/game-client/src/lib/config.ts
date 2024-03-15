6c437793b114dd2b8b1e5f8536677ecb57781549
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-03-15 06:04:51 +0200

    fix: pre refactor
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- libs/game-client/src/lib/config.ts
+++ libs/game-client/src/lib/config.ts
@@ -1,3 +1,5 @@
+import * as process from 'process';
+
 export const NODE_ENV = process.env['NODE_ENV'];
 export enum NODE_ENV_STATE {
   PRODUCTION = 'production',
@@ -6,8 +8,11 @@ export enum NODE_ENV_STATE {
 }
 const isProduction = NODE_ENV === NODE_ENV_STATE.PRODUCTION;
 export const SERVER_KEY = 'defaultkey',
-  HOST = isProduction ? 'nakama.5ostudios.com' : 'localhost',
-  PORT = isProduction ? '' : '7350',
-  USE_SSL = isProduction,
+  HOST =
+    process.env['NAKAMA_HOST'] || isProduction
+      ? 'nakama.5ostudios.com'
+      : 'localhost',
+  PORT = process.env['NAKAMA_PORT'] || isProduction ? '' : '7350',
+  USE_SSL = process.env['NAKAMA_SSL'] === 'true' || isProduction,
   TIMEOUT = 7500,
   AUTO_REFRESH_SESSION = true;
