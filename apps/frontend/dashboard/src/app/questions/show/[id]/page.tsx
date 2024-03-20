0c11fc81a06e8c8b15a7b730069874028d4250c7
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-03-20 08:53:21 +0200

    feat: core lib
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/frontend/dashboard/src/app/questions/show/[id]/page.tsx
+++ apps/frontend/dashboard/src/app/questions/show/[id]/page.tsx
@@ -1,18 +1,16 @@
-"use client";
-
+'use client';
 import {
   DateField,
   MarkdownField,
   NumberField,
   Show,
   TextField,
-} from "@refinedev/antd";
-import { useOne, useShow } from "@refinedev/core";
-import { Typography } from "antd";
-import React from "react";
+} from '@refinedev/antd';
+import { useOne, useShow } from '@refinedev/core';
+import { Typography } from 'antd';
+import React from 'react';
 
 const { Title } = Typography;
-
 export default function BlogPostShow() {
   const { queryResult } = useShow({});
   const { data, isLoading } = queryResult;
@@ -20,8 +18,8 @@ export default function BlogPostShow() {
   const record = data?.data;
 
   const { data: categoryData, isLoading: categoryIsLoading } = useOne({
-    resource: "categories",
-    id: record?.category?.id || "",
+    resource: 'categories',
+    id: record?.category?.id || '',
     queryOptions: {
       enabled: !!record,
     },
@@ -29,21 +27,21 @@ export default function BlogPostShow() {
 
   return (
     <Show isLoading={isLoading}>
-      <Title level={5}>{"ID"}</Title>
-      <NumberField value={record?.id ?? ""} />
-      <Title level={5}>{"Title"}</Title>
+      <Title level={5}>{'ID'}</Title>
+      <NumberField value={record?.id ?? ''} />
+      <Title level={5}>{'Title'}</Title>
       <TextField value={record?.title} />
-      <Title level={5}>{"Content"}</Title>
+      <Title level={5}>{'Content'}</Title>
       <MarkdownField value={record?.content} />
-      <Title level={5}>{"Category"}</Title>
+      <Title level={5}>{'Category'}</Title>
       <TextField
         value={
           categoryIsLoading ? <>Loading...</> : <>{categoryData?.data?.title}</>
         }
       />
-      <Title level={5}>{"Status"}</Title>
+      <Title level={5}>{'Status'}</Title>
       <TextField value={record?.status} />
-      <Title level={5}>{"CreatedAt"}</Title>
+      <Title level={5}>{'CreatedAt'}</Title>
       <DateField value={record?.createdAt} />
     </Show>
   );
