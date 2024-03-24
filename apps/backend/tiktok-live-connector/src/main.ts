4aa691efd8369b3188d53b00e4f6c2be5a46e99e
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-03-24 12:27:04 +0200

    build: optimize tiktok
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/tiktok-live-connector/src/main.ts
+++ apps/backend/tiktok-live-connector/src/main.ts
@@ -5,7 +5,6 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { AsyncApiDocumentBuilder, AsyncApiModule } from 'nestjs-asyncapi';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,23 +12,23 @@ async function bootstrap() {
   const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 4444;
-  const asyncApiOptions = new AsyncApiDocumentBuilder()
-    .setTitle('Kingo Backend')
-    .setDescription('The Kingo Backend API')
-    .setVersion('1.0')
-    .setDefaultContentType('application/json')
-    .addServer('TikTok Live Connector', {
-      url: `ws://localhost:${port}`,
-      protocol: 'socket.io',
-    })
-    .build();
-
+  // const asyncApiOptions = new AsyncApiDocumentBuilder()
+  //   .setTitle('Kingo Backend')
+  //   .setDescription('The Kingo Backend API')
+  //   .setVersion('1.0')
+  //   .setDefaultContentType('application/json')
+  //   .addServer('TikTok Live Connector', {
+  //     url: `ws://localhost:${port}`,
+  //     protocol: 'socket.io',
+  //   })
+  //   .build();
+  //
   app.enableCors({
     origin: '*',
   });
-
-  const asyncApiDocument = AsyncApiModule.createDocument(app, asyncApiOptions);
-  await AsyncApiModule.setup(globalPrefix, app, asyncApiDocument);
+  //
+  // const asyncApiDocument = AsyncApiModule.createDocument(app, asyncApiOptions);
+  // await AsyncApiModule.setup(globalPrefix, app, asyncApiDocument);
 
   await app.listen(port);
   Logger.log(
