2164ad8178c69c66c9b6351e0d918b144f7d9bb8
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-07-22 11:58:13 +0300

    fix: clear timer
    Signed-off-by: Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>

--- apps/backend/game-server/src/modules/claws/commands/end-turn.command.ts
+++ apps/backend/game-server/src/modules/claws/commands/end-turn.command.ts
@@ -6,7 +6,7 @@ import { SelectNextPlayerCommand } from './next-player.command';
 export class EndTurnCommand extends Command<ClawsRoom> {
   async execute() {
     this.state.currentPlayer.endTurn();
-    this.clock.clear();
+    // this.clock.clear();
     console.log('Player turn ended ', this.state.currentPlayer.sessionId);
     await this.room.dispatcher.dispatch(new SelectNextPlayerCommand());
   }
