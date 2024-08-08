69262b3f91a93be18ba41f43e863030c4a138fdc
Abdelrahman Essawy <abdelrahman.mo.essawy@gmail.com>
2024-08-08 18:42:32 +0300

    fix: ts error

--- libs/core/src/lib/appInfo.ts
+++ libs/core/src/lib/appInfo.ts
@@ -1,8 +1,11 @@
+const apiDomain =
+  process.env['NEXT_PUBLIC_BACKEND_URL'] ?? 'http://localhost:3000';
+const websiteDomain = process.env['FRONTEND_URL'] ?? 'http://localhost:3001';
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: 'Kingo',
-  apiDomain: process.env['NEXT_PUBLIC_BACKEND_URL'],
-  websiteDomain: process.env['FRONTEND_URL'],
+  apiDomain,
+  websiteDomain,
   apiBasePath: '/auth',
   websiteBasePath: '/api/auth',
 };
